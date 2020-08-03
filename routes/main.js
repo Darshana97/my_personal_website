bf58a2656ee18aad8f39eccfee76ca3141528c5b
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-08-03 22:52:22 +0530

    add languages

--- routes/main.js
+++ routes/main.js
@@ -4,7 +4,14 @@ const router = express.Router();
 router.get("/", (req, res) => {
   const data = {
     greeting: "Hello Welcome to my portfolio site!",
-    introduction: "I am web developer from Kandy"
+    introduction: "I am web developer from Kandy",
+    languages: [
+      {name: 'javascript', years: 3},
+      {name: 'Angular', years: 2},
+      {name: 'NodeJS', years: 2},
+      {name: 'AWS', years: 1},
+      
+    ]
   };
 
   res.render("landing", data);
